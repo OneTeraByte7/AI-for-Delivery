97edd381554af84804bf599bcdecf3318b3ef961
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-08-13 21:34:44 +0530

    Edited:test script

--- src/test_env.py
+++ src/test_env.py
@@ -1,15 +1,25 @@
 from env import DeliveryFleetEnv
+import random
+import time
 
 def main():
-    env = DeliveryFleetEnv(grid_size=5, num_agents=2)
+    env = DeliveryFleetEnv(grid_size=6, num_agents=2, max_orders=3, order_spawn_rate=3)
     obs = env.reset()
     print("Initial Observations:", obs)
+    print("Inital Orders:", env.orders)
     env.render()
 
-    actions = {'agent_0': 4, 'agent_1': 1}  # agent_0 moves right, agent_1 moves up
-    obs, rewards, dones, infos = env.step(actions)
-    print("Rewards:", rewards)
-    env.render()
+    for step in range(10):   
+        actions = {agent: random.randint(0, 4) for agent in env.agents}
+        obs, rewards, dones, infos = env.step(actions)
+        
+        print(f"\n--- Step {step+1}---")
+        env.render()
+        print("Actions:", actions)
+        print("Rewards:", rewards)
+        print("Active Orders:", env.orders)
+        
+        time.sleep(0.3)
 
 if __name__ == "__main__":
     main()
