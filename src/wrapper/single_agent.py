60183ccea3142cce4b0a8c84dd8ac2007248a015
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-08-19 10:16:55 +0530

    refined:ppo agent

--- src/wrapper/single_agent.py
+++ src/wrapper/single_agent.py
@@ -10,13 +10,15 @@ class SingleAgentWrapper(gym.Env):
 
     metadata = {"render_modes": []}
 
-    def __init__(self, base_env_cls, env_kwargs=None, control_agent="agent_0"):
+    def __init__(self, base_env_cls, env_kwargs=None, control_agent="agent_0", max_episode_steps=100):
         super().__init__()
         env_kwargs = env_kwargs or {}
         self.base_env = base_env_cls(**env_kwargs)
         self.control_agent = control_agent
+        self._t = 0
+        self.max_episode_steps = max_episode_steps
 
-        # ----- Observation space: sample a real observation -----
+        # ----- Observation space -----
         sample_obs = np.array(self.base_env.reset()[self.control_agent], dtype=np.float32)
         self.observation_space = gym.spaces.Box(
             low=-np.inf * np.ones_like(sample_obs, dtype=np.float32),
@@ -25,26 +27,26 @@ class SingleAgentWrapper(gym.Env):
         )
 
         # ----- Action space -----
-        act_space = getattr(self.base_env, "action_spaces", {}).get(self.control_agent,None)
+        act_space = getattr(self.base_env, "action_spaces", {}).get(self.control_agent, None)
         if isinstance(act_space, int):
             self.action_space = gym.spaces.Discrete(act_space)
-        
         elif act_space is not None:
             self.action_space = act_space
         else:
             try:
                 self.action_space = self.base_env.action_space(self.control_agent)
-                
             except Exception:
                 self.action_space = gym.spaces.Discrete(5)
 
     def reset(self, *, seed=None, options=None, **kwargs):
-        self._t += 1
+        self._t = 0
         obs = self.base_env.reset()
         return np.array(obs[self.control_agent], dtype=np.float32), {}
 
     def step(self, action):
         self._t += 1
+
+        # Build full action dict
         actions = {}
         for agent in self.base_env.agents:
             if agent == self.control_agent:
@@ -53,7 +55,6 @@ class SingleAgentWrapper(gym.Env):
                 space = getattr(self.base_env, "action_spaces", {}).get(agent, None)
                 if isinstance(space, int):
                     actions[agent] = np.random.randint(0, space)
-                    
                 elif space is not None:
                     actions[agent] = space.sample()
                 else:
@@ -62,11 +63,8 @@ class SingleAgentWrapper(gym.Env):
         obs, rewards, dones, infos = self.base_env.step(actions)
 
         terminated = dones.get(self.control_agent, False)
-        truncated = False 
-        
-        if self.max_episode_step is not None and self._t >= self.max_episode_steps:
-            truncated = True
-            
+        truncated = self._t >= self.max_episode_steps
+
         info = infos.get(self.control_agent, {})
 
         return (
