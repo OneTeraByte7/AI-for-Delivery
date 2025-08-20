46c1bdd66263cb759c056f9b0d2c3481350c6d39
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-08-20 21:09:10 +0530

    function:main for multi agent

--- src/wrapper/multi_agent.py
+++ src/wrapper/multi_agent.py
@@ -7,3 +7,23 @@ class MultiAgentWrapper(gym.Env):
     for multi-agent PPO training. Observations are concatenated for all agents.
     """
     metadata = {"render_modes": []}
+    
+    def __init__ (self, base_env_cls, env_kwargs=None):
+        super().init__()
+        env_kwargs = env_kwargs or {}
+        self.base_env = base_env_cls(**env_kwargs)
+        self.agents = self.base_env.agents
+        
+        
+        sample_obs = np.concantenate(
+            [np.array(self.base_env.reset()[agent], dtype=np.float32) for agent in self.agents]
+        )
+        
+        self.observations_space = gym.spaces.Box(
+            low =- np.inf * np.ones_like(sample_obs, dtype = np.float32),
+            high = np.inf * np.ones_like(sample_obs, dtype = np.float32),
+            dtype = np.float32,
+        )
+        
+        max_actions = max([self.base_env.action_spaces[agent] for agent in self.agents])
+        self.action_space = gym.spaces.MultiDiscrete([max_actions for _ in self.agents])
