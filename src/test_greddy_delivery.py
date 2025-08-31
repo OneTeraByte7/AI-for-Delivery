43e7774a61bdb6cacdacd20b85db9b329ec5b61d
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-08-31 14:30:58 +0530

    greedy: logic

--- src/test_greddy_delivery.py
+++ src/test_greddy_delivery.py
@@ -3,4 +3,15 @@ from env import DeliveryFleetEnv
 
 def greedy_policy(obs, env, agent_id):
     """Decide action based on a greedy policy."""
-    agents_pos = env.agent_position[agent_id]
+    agent_pos = env.agent_positions[agent_id]
+    
+    if env.agent_carrying[agent_id]:
+        targets = env.deliveries
+        
+    else:
+        targets = env.orders
+        
+    if not targets:
+        return 0
+    
+    target = min(targets, key=lambda t: abs(t[0] - agent_pos[0]))
